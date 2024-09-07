d08d535c95b43a2e03396863c8acbb244b2cd649
anuyiohc <149288545+anuyiohc@users.noreply.github.com>
2024-09-07 11:54:48 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+//js가 동적으로 제어하는 부분
+</script>
+
+<!-- html -->
+<template>
+  <h1>TODO LIST</h1>
+
+  <input />
+  <button>확인</button>
+
+  <div class="todo-item">
+  <div>코드짜기</div>
+  <button>수정</button>
+  </div>
+
+</template>
+
+<!-- css -->
+<style scoped>
+.todo-item{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border:1px solid gray;
+}
+</style>
