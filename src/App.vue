c57f2ce487067add33ab4a89f499ced62149705b
anuyiohc <149288545+anuyiohc@users.noreply.github.com>
2024-09-07 12:02:50 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
+<!-- js  -->
 <script setup>
-//js가 동적으로 제어하는 부분
+import { ref } from 'vue';
+/**
+  todo item
+  key: text, done
+  item.text or item['text']
+  {
+    text: string
+    done: boolean(true/false)
+  }
+*/
+const inputValue = ref('');
+const todoList = ref([
+  { text: 'vue', done: false },
+  { text: 'react', done: false },
+  { text: 'vercel', done: false },
+]);
+
+const handleClickButton = () => {
+  todoList.value.push(inputValue.value);
+};
+
+const handleChange = (event) => {
+  const nextValue = event.target.value;
+  inputValue.value = nextValue;
+};
+
+const handleClickDeleteButton = (index) => {
+  todoList.value.splice(index, 1);
+};
 </script>
 
 <!-- html -->
 <template>
-  <h1>TODO LIST</h1>
+  <p>{{ todoList }}</p>
+  <h4>TODO LIST</h4>
 
-  <input />
-  <button>확인</button>
+  <input v-model="inputValue" />
+  <!-- <input :value="inputValue" @change="handleChange" /> -->
 
-  <div class="todo-item">
-  <div>코드짜기</div>
-  <button>수정</button>
+  <button @click="handleClickButton">확인</button>
+
+  <div class="todo-item" v-for="(item, index) in todoList">
+    <input type="checkbox" v-model="item.done" />
+    <span :class="{ 'done-item': item.done }">{{ item.text }}</span>
+    <!-- item.done ? <span class="done-item"... : 
+                      <span class="" ...
+    -->
+    <div>
+      <!-- <button>수정</button> -->
+      <button @click="handleClickDeleteButton(index)">삭제</button>
+    </div>
   </div>
-
 </template>
 
 <!-- css -->
 <style scoped>
-.todo-item{
+.todo-item {
+  width: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  border:1px solid gray;
+  border: 1px solid gray;
+}
+
+.done-item {
+  text-decoration: line-through;
 }
 </style>
